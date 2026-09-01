97c1baf94542163ca5fe72cc99868bd7c38ac1df
lakshya pandey <er.pklakshya@gmail.com>
2026-09-01 12:20:29 +0530

    Add files via upload

---
+++ day2/script.js
@@ -0,0 +1,11 @@
+let name = "Lakshya"
+let last = new String()
+last = "pandey"
+let email = "er.pklakshya@gmail.com"
+
+console.log(typeof name)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.charAt(3))
+console.log(email.indexOf('@'))
+console.log(email.lastIndexOf('.'))
