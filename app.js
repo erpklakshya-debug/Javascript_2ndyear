da2b84d0071cc9a887b3ea60f3ef00c4ba6ce44f
lakshya pandey <er.pklakshya@gmail.com>
2026-09-01 12:19:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const xx = 10;
+const yy = 20;
+
+module.exports = { xx, yy };
---
+++ index.js
@@ -0,0 +1,16 @@
+// var a = 40;
+// var b = 20;
+// var c = 30;
+// console.log(a+b+c);
+
+// var x = 20;
+// const arr =[1,2,3,4,5];
+// for(var i =0; i<5; i++){
+    // console.log(arr[i]);
+    
+
+const app=require("./app.js");
+console.log(app.xx);
+console.log(app.yy);
+
+console
